7d9b48ed4df83876fa9bf018e1cd848a0ff6c96a
Abayomi Ayodele <samabayomi301@gmail.com>
2022-12-27 10:17:35 +0100

    setup custom logger middleware

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 
+// custom middleware
+import logger from "./middlewares/logger.js";
+
 // router files
 import BootcampRouter from "./routes/bootcamps.js";
 
@@ -9,6 +12,9 @@ dotenv.config({ path: "./config/config.env" });
 
 // initialize app
 const app = express();
+
+// setup custom middlewares
+app.use(logger);
 
 // setup routes
 app.use("/api/v1/bootcamps", BootcampRouter);
