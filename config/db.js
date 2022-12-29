79f9066e2c0993f4750a31e69aa64e0c805f8940
Abayomi Ayodele <samabayomi301@gmail.com>
2022-12-29 10:28:00 +0100

    setup mongodb connection with error handling

---
+++ config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+mongoose.set("strictQuery", false);
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`MongoDB connected on: ${conn.connection.host}`);
+  } catch (err) {
+    console.log("Error: ", err.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- server.js
+++ server.js
@@ -2,16 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 
+import connectDB from "./config/db.js";
+
 // router files
 import BootcampRouter from "./routes/bootcamps.js";
-
-// load env files
-dotenv.config({ path: "./config/config.env" });
 
 // initialize app
 const app = express();
 
-// setup custom middlewares for dev login
+// load env files
+dotenv.config({ path: "./config/config.env" });
+
+// setup custom middlewares for dev request logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
@@ -19,9 +21,12 @@ if (process.env.NODE_ENV === "development") {
 // setup routes
 app.use("/api/v1/bootcamps", BootcampRouter);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8888;
+
+// connect to DB
+connectDB();
 
 app.listen(
   PORT,
-  console.log(`Server setup in ${process.env.NODE_ENV} on port: ${PORT}`)
+  console.log(`Local server setup in ${process.env.NODE_ENV} on port: ${PORT}`)
 );
